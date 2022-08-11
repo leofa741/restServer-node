34d27de30a6bc16d4a2757044d90f98b12708839
leofa741 <61668570+leofa741@users.noreply.github.com>
2022-08-10 21:42:03 -0300

    login google

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -2,15 +2,16 @@ const { OAuth2Client } = require('google-auth-library');
 
 const client = new OAuth2Client( process.env.GOOGLE_CLIENT_ID );
 
-const googleVerify = async( idToken = '' ) => {
+const googleVerify = async( Token = '' ) => {
 
   const ticket = await client.verifyIdToken({
-      idToken,
+      idToken: Token,
+    
       audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
-
+  console.log(idToken);
   const { name: nombre, 
           picture: img, 
           email: correo
